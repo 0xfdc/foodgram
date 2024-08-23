54cd7d79cce5b0e53334366bbfa6aeed4504a885
Arch <soulyraziel@gmail.com>
2024-08-23 19:05:16 +0300

    custom django-admin commands for data loading

---
+++ backend/recipes/management/commands/load_ingredients.py
@@ -0,0 +1,20 @@
+import csv
+
+from django.conf import settings
+from django.core.management import BaseCommand
+
+from recipes.models import Ingredient
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **kwargs):
+        data_path = settings.BASE_DIR
+        with open(f'{data_path}/data/ingredients.csv', 'r',
+                  encoding='utf-8') as file:
+            reader = csv.reader(file)
+            for row in reader:
+                Ingredient.objects.create(
+                    name=row[0], measurement_unit=row[1]
+                )
+        self.stdout.write(self.style.SUCCESS('Ингредиенты добавлены'))
---
+++ backend/recipes/management/commands/load_tags.py
@@ -0,0 +1,17 @@
+from django.core.management import BaseCommand
+
+from recipes.models import Tag
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **kwargs):
+        data = [
+            {'name': 'Завтрак', 'slug': 'breakfast'},
+            {'name': 'Обед', 'slug': 'lunch'},
+            {'name': 'Ужин', 'slug': 'dinner'},
+            {'name': 'Праздник', 'slug': 'holiday'},
+            {'name': 'Быстро', 'slug': 'fast'},]
+        for tag in data:
+            Tag.objects.create(name=tag['name'], slug=tag['slug'])
+        self.stdout.write(self.style.SUCCESS('Теги добавлены'))
